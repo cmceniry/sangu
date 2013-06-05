a8f38b92e6ed54ff0999bfedb62afa39622f320b
Chris McEniry <cmceniry@corgalabs.com>
2013-06-04 23:52:23 -0700

    * move bootstrap images to the default place * helper script to pull current dependency js libs

---
+++ scripts/fetch-libs.sh
@@ -0,0 +1,17 @@
+
+curl -o \
+  resources/public/components/wiky.js \
+  http://goessner.net/download/prj/wiky/wiky.js
+
+curl -o \
+  resources/public/components/bootstrap-combined-2-3-2.min.css \
+  http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+
+curl -o \
+  resources/public/img/glyphicons-halflings.png \
+  http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/img/glyphicons-halflings.png
+
+curl -o \
+  resources/public/components/angular.js \
+  https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js
+
